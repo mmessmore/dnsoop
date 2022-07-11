5031b56462a01ffbfd91ed9adddee964f89c32cf
Michael Messmore <mike@messmore.org>
2022-07-11 11:01:57 -0500

    sort by value in output, do counts by src for hostname

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"sort"
 	"strconv"
 	"strings"
 	"syscall"
@@ -19,6 +20,8 @@ import (
 
 var iface = flag.String("i", "eth0", "Network interface to capture on")
 var verbose = flag.Bool("v", false, "Verbose mode")
+var width = flag.Int("w", 80, "Terminal column width for printing tables")
+var onlyHostname = flag.String("H", "", "Count sources for a hostname, vs hostname counts")
 var snapLen int32 = 65535
 var filter = "udp and dst port 53"
 
@@ -57,11 +60,62 @@ func getHost(packet *gopacket.Packet) (string, error) {
 	return "", errors.New("No A record query")
 }
 
+func getSrc(packet *gopacket.Packet, hostname *string) (string, error) {
+	p := *packet
+	src := "none"
+
+	if ipv4 := p.Layer(layers.LayerTypeIPv4); ipv4 != nil {
+		layer := ipv4.(*layers.IPv4)
+		src = layer.SrcIP.String()
+	} else {
+		return "", errors.New("Not an IPv4 packet")
+	}
+
+	if !hasDNSLayer(packet) {
+		return "", errors.New("Not a dns packet")
+	}
+
+	dnsLayer := p.Layer(layers.LayerTypeDNS).(*layers.DNS)
+
+	if dnsLayer.OpCode != layers.DNSOpCodeQuery {
+		return "", errors.New("Not a dns query")
+	}
+
+	for _, question := range dnsLayer.Questions {
+		if question.Type != layers.DNSTypeA {
+			continue
+		}
+		if string(question.Name) != *hostname {
+			return "", errors.New("No the hostname we were looking for")
+		}
+	}
+
+	return src, nil
+}
+
 func dumpCounts(queryCnt *map[string]int) {
-	maxWidth := 40
+
+	// Jump through hoops to sort the map by value into a slice for odering
+	type kv struct {
+		Key   string
+		Value int
+	}
+	var ss []kv
 	for k, v := range *queryCnt {
-		spaces := maxWidth - len(k) - len(strconv.Itoa(v))
-		fmt.Printf("%s%s%d\n", k, strings.Repeat(" ", spaces), v)
+		ss = append(ss, kv{k, v})
+	}
+	sort.Slice(ss, func(i, j int) bool {
+		return ss[i].Value > ss[j].Value
+	})
+
+	// Walk through the sorted slice and print them all pretty-like
+	maxWidth := *width
+	for _, item := range ss {
+		spaces := maxWidth - len(item.Key) - len(strconv.Itoa(item.Value))
+		if spaces < 0 {
+			spaces = 10
+		}
+		fmt.Printf("%s%s%d\n", item.Key, strings.Repeat(" ", spaces), item.Value)
 	}
 }
 
@@ -99,15 +153,21 @@ func main() {
 	ticker := time.Tick(time.Minute)
 
 	for {
+		host := "none"
 		select {
 		case packet := <-packets:
-			host, err := getHost(&packet)
+			if *onlyHostname == "" {
+				host, err = getHost(&packet)
+			} else {
+				host, err = getSrc(&packet, onlyHostname)
+			}
+
 			if err != nil {
 				continue
 			}
 
 			if _, ok := queryCnt[host]; ok {
-				queryCnt[host] += 1
+				queryCnt[host]++
 			} else {
 				queryCnt[host] = 1
 			}
